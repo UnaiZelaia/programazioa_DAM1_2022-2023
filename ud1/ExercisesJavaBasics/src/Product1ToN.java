10885317abd091a9bd08f04c6b650b35419befa1
Unai Zelaia-Zugadi <unaizelaiaz@gmail.com>
2022-09-26 14:28:01 +0200

    Cambios en Product1ToN

---
+++ ud1/ExercisesJavaBasics/src/Product1ToN.java
@@ -0,0 +1,12 @@
+public class Product1ToN{
+    public static void main(String[] args) {
+        final int LOWERBOUND = 1;
+        final int UPPERBOUND = 10;
+        int product = 1;
+
+        for(int number = LOWERBOUND; number < UPPERBOUND; number ++){
+            product = product * number;
+            System.out.println(product);
+        }
+    }
+}
